2abe4160e3998662ebede7a8614164473deabb5d
Ivan <ivantieplih@gmail.com>
2026-03-25 16:37:39 +0200

    23

--- app/notes/filter/[[...tag]]/page.tsx
+++ app/notes/filter/[[...tag]]/page.tsx
@@ -1,6 +1,10 @@
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { fetchNotes } from '../../../lib/api';
-import NotesClient from '../../Notes.client';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { fetchNotes } from "../../../../lib/api";
+import NotesClient from "../../Notes.client";
 
 interface FilterPageProps {
   params: {
@@ -10,13 +14,13 @@ interface FilterPageProps {
 
 export default async function FilterPage({ params }: FilterPageProps) {
   const { tag: tagParams } = params;
-  const tag = tagParams && tagParams[0] !== 'all' ? tagParams[0] : undefined;
-  
+  const tag = tagParams && tagParams[0] !== "all" ? tagParams[0] : undefined;
+
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['notes', '', 1, tag],
-    queryFn: () => fetchNotes({ search: '', page: 1, perPage: 12, tag }),
+    queryKey: ["notes", "", 1, tag],
+    queryFn: () => fetchNotes({ search: "", page: 1, perPage: 12, tag }),
   });
 
   return (
